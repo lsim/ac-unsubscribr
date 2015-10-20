28d5c30b5738baf23d41ca99b4eb4787a38ae8af
Constantin Gavrilete <ccg@c3a.dk>
2015-10-20 23:42:13 +0100

    some stuff

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -3,4 +3,40 @@
 *
 * Description
 */
-angular.module('app', []);
+var app = angular.module('app', ['ngRoute', 'AdalAngular']);
+
+app.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalAuthenticationServiceProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/home.html',
+			controller: 'HomeController',
+			controllerAs: 'home',
+			requireADLogin: true
+		})
+
+		.otherwise({
+			redirectTo: '/'
+		});
+
+	var endpoints = {
+		'https://outlook.office365.com': 'https://outlook.office365.com'
+	};
+
+	adalAuthenticationServiceProvider.init({
+		// tenant: 'dxdev01.onmicrosoft.com',
+		clientId: 'e5648932-409f-48ee-a297-611c375592df',
+		endpoints: endpoints,
+		cacheLocation: 'localStorage'
+	}, $httpProvider);
+	console.log("done");
+}]);
+
+app.controller('HomeController', ['$http', function ($http) {
+	$http.get("https://outlook.office365.com/api/v1.0/me/messages").then(function (response) {
+		console.log("success");
+		console.log(response);
+	}, function (error) {
+		console.log("error");
+		console.log(error);
+	});
+}]);
